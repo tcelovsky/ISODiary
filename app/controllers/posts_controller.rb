9bf99a74d406d3db18ff7bed2429e5f2e87df3c7
tscotce <tatyana.celovsky@gmail.com>
2020-04-20 15:47:14 -0400

    Added a /posts route.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,9 @@
 class PostsController < ApplicationController
+
+    get '/posts' do
+        if logged_in?
+            erb :'/posts/index'
+        else redirect to :'/login'
+        end
+    end
 end
