b0a6aa954db08cef8e1511a79a1001ae9ba8684b
tscotce <tatyana.celovsky@gmail.com>
2020-04-30 17:02:14 -0400

    Multiple edits in accordance with initial review meeting.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,13 +10,13 @@ class ApplicationController < Sinatra::Base
 
     helpers do
         def current_user
-            # find the current user by user id in the session hash
-            @current_user ||= User.find_by(id: session[:id]) if session[:id]
-          end
+          # find the current user by user id in the session hash
+          @current_user ||= User.find_by(id: session[:id]) if session[:id]
+        end
       
-          def logged_in?
-            # check that the user is logged in by making sure that the session hash exists
-            !!current_user
-          end
+        def logged_in?
+          # check that the user is logged in by making sure that the session hash exists
+          !!current_user
+        end
     end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,62 +1,66 @@
 class PostsController < ApplicationController
 
     get '/posts' do
-        if logged_in?
+        if logged_in? 
             erb :'/posts/index'
         else redirect to :'/login'
         end
     end
 
     get '/posts/new' do
-        if logged_in?
+        if logged_in? 
             erb :'/posts/new'
         else redirect to :'/login'
         end
     end
 
-    post '/posts/new' do
+    post '/posts' do
+        # the route should be '/posts' not '/posts/new' - DONE
         if params[:title].present? && params[:content].present?
             @user = current_user
             @post = Post.create(title: params[:title], content: params[:content])
             @post.user_id = @user.id
             @post.save
-            erb :'/posts/show'
-          else redirect to :'/posts/new'
-          end
+            redirect to :"/posts/#{@post.id}" 
+            # add post id from params here. - DONE
+        else erb :'/posts/new'
+        end
     end
 
     get '/posts/:id' do
-        if logged_in?
-            @post = Post.find_by_id(params[:id])
+        @post = Post.find_by_id(params[:id])
+        if @post.user_id == session[:id]
             erb :'/posts/show'
-          else redirect to :'/login'
-          end
+        else redirect to :'/login'
+        end
     end
 
     delete '/posts/:id/delete'do
-        if logged_in?
+        if logged_in? 
             @post = Post.find_by(params[:id])
             @post.delete
+            # check why the post is not being deleted. - SEEMS TO BE WORKING
             erb :'/posts/index'
         else redirect to :'/login'
         end
     end
 
     get '/posts/:id/edit' do
-        if logged_in?
-            @post = Post.find_by_id(params[:id])
+        i@post = Post.find_by_id(params[:id])
+        if @post.user_id == session[:id]
             erb :'/posts/edit'
         else redirect to :'/login'
         end
     end
 
-    patch '/posts/:id/edit' do
+    patch '/posts/:id' do
         if params[:title].present? && params[:content].present?
             @post = Post.find_by_id(params[:id])
             @post.title = params[:title]
             @post.content = params[:content]
             @post.save
-            erb :'/posts/index'
+            redirect to :"/posts/#{@post.id}"
+            # should redirect to '/posts/show' - DONE
         else erb :'/posts/edit'
         end
     end
